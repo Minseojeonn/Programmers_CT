184f5fd64311594f34143bbb616c3ef54be03121
miseo_jeon <83704225+Minseojeonn@users.noreply.github.com>
2026-02-11 21:55:29 +0900

    [level 2] Title: 배달, Time: 11.35 ms, Memory: 9.6 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -1,36 +1,33 @@
+# num_vil = N, roads, hold=K
 def solution(N, road, K):
-    
-    edges = {}
-    
-    for i in range(N+1):
-        edges[i] = []
-    
-    for roa in road:
-        fr, to, cost = roa
-        edges[fr].append([to, cost])
-        edges[to].append([fr, cost])
-        
-    time_cost = [float('inf') for _ in range(N+1)]
-    time_cost[1] = 0
-    
-    queue = [1]
-    
-    while queue:
-        start_point = queue.pop()
-        destination = edges[start_point]
-        for dest in destination:
-            de, co = dest
-            new_cost = time_cost[start_point] + co
-            if new_cost <= time_cost[de]:
-                queue.append(de)
-                time_cost[de] = new_cost
-    
+    graph = {}
+    min_cost = [9999999 for _ in range(N+1)] # 0 - no avail
+    min_cost[1] = 0
+    stack = []
+    for ro in road:
+        fr, to, val = ro
+        if fr not in graph:
+            graph[fr] = [(to,val)]
+        else:
+            graph[fr].append((to,val))
+        if to not in graph:
+            graph[to] = [(fr,val)]
+        else:
+            graph[to].append((fr,val))
+    stack.append(1)
+    while stack:
+        cur_vil = stack.pop()
+        connected_vils = graph[cur_vil]
+        for connected_vil in connected_vils:
+            connected_vil_num, connected_vil_cost = connected_vil
+            if min_cost[cur_vil] + connected_vil_cost <= min_cost[connected_vil_num]:
+                min_cost[connected_vil_num] = min_cost[cur_vil] + connected_vil_cost
+                stack.append(connected_vil_num)
     answer = 0
-    for val in time_cost:
-        if val <= K:
-            answer += 1
+    for i in min_cost[1:]:
+        if i <= K:
+            answer +=1
+        
+
     return answer
 
-# 1. undirect 로 모든 edge를 저장하고
-# 2. 1부터 while문으로 반복종료까지 아래 시도
-#업데이트가 되면, 연결된 지점들도 다 업데이트 시도하기.
