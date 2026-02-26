bd05075e4b00f3a6ce5e1acb2b6f7dbf5477aa61
miseo_jeon <83704225+Minseojeonn@users.noreply.github.com>
2026-02-26 13:01:31 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 288 ms, Memory: 127504 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,25 +1,23 @@
-import sys
-from collections import defaultdict
 from collections import deque
+num_nodes = int(input())
 
-num_edges = int(sys.stdin.readline().strip())
-edge_checker = {}
-edge_dict = defaultdict(list)
+adj = [[] for i in range(num_nodes+1)]
+root = [-1 for i in range(num_nodes+1)]
+visited = [0 for i in range(num_nodes+1)]
+for i in range(num_nodes-1):
+    fr, to = map(int, input().split(" "))
+    adj[fr].append(to)
+    adj[to].append(fr)
 
-for i in range(num_edges-1):
-    edge_checker[i] = False
-    fr, to = sys.stdin.readline().strip().split()
-    edge_dict[fr].append((to, i))
-    edge_dict[to].append((fr, i))
 
-queue = deque(['1'])
-tree = {}
+queue = deque([1])
+visited[1] = 1
 while queue:
-    next = queue.popleft() 
-    for candidate in edge_dict[next]:
-        if edge_checker[candidate[1]] == False:
-            edge_checker[candidate[1]] = True
-            queue.append(candidate[0])
-            tree[candidate[0]] = next
-for i in range(2, num_edges+1):
-    print(tree[str(i)])
+    next_node = queue.popleft()
+    for cand_node in adj[next_node]:
+        if visited[cand_node] == 0:
+            root[cand_node] = next_node
+            queue.append(cand_node)
+            visited[cand_node] = 1
+for i in root[2:]:
+    print(i)
